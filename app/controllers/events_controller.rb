d7a9c1bf89a9d4e10b6db17219669c297e77940e
Aisyah Zulkifli <aisyahqiilah@gmail.com>
2024-12-01 21:24:39 -0500

    Initial skeleton with events

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,24 @@
+class EventsController < ApplicationController
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to @event, notice: "Event created successfully!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description, :date)
+  end
+end
